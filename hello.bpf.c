06a5886e4243e95233a15da4094b81646c2f7484
Hamidreza Khosroabadi <hamidreza.khosroabadi@iau.ir>
2026-07-24 18:06:09 +0000

    feat: add details of event to k print

--- hello.bpf.c
+++ hello.bpf.c
@@ -1,11 +1,18 @@
-#include <linux/bpf.h>
+#include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
 
 char LICENSE[] SEC("license") = "GPL";
 
 SEC("tp/syscalls/sys_enter_write")
-int hello(void *ctx)
+int hello(struct trace_event_raw_sys_enter *ctx)
 {
-    bpf_printk("Hello, eBPF World! Someone called write()\n");
+    int fd = (int)ctx->args[0];
+    const char *ubuf = (const char *)ctx->args[1];
+    __u64 count = (__u64)ctx->args[2];
+
+    char buf[64] = {};
+    bpf_probe_read_user_str(buf, sizeof(buf), ubuf);
+    bpf_printk("fd=%d count=%llu data=%s", fd, count, buf);
     return 0;
 }
